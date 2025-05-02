4a538b9b3c842cf015a6b4d567d840c7ae21de29
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-05-02 09:50:22 +0200

    basic server config added +logging + routing structure

--- server.js
+++ server.js
@@ -1,25 +1,42 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 module.exports = app;
 
+// Turn on all logging in file
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "./logs/access.log"),
+  { flags: "a" } // 'a' will write to the end of file
+);
+
+app.use(morgan("combined", { stream: accessLogStream }));
+//TODO: In the future, may be use rotate-file-stream option!
+
 /* Do not change the following line! It is required for testing and allowing
-*  the frontend application to interact as planned with the api server
-*/
+ *  the frontend application to interact as planned with the api server
+ */
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
+const cors = require("cors");
+app.use(cors());
 
-
-// Add middware for parsing request bodies here:
-
+// Add middleware for parsing request bodies here:
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
 // Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require('./server/api');
-
+const apiRouter = require("./server/api");
+app.use("/api", apiRouter);
 
 // This conditional is here for testing purposes:
-if (!module.parent) { 
+if (!module.parent) {
   // Add your code to start the server listening at PORT below:
-
+  app.listen(PORT, () => {
+    console.log(`Server is started on port ${PORT}`);
+  });
 }
--- server/api.js
+++ server/api.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
+// Use morgan for logging
+const morgan = require("morgan");
 
+apiRouter.use(morgan("dev"));
 
+//import main routers files for minions, ideas and meetings entity
+const minionsRouter = require("./routers/minions");
+const ideasRouter = require("./routers/ideas");
+const meetingsRouter = require("./routers/meetings");
+
+//use our routers
+apiRouter.use("/minions", minionsRouter);
+apiRouter.use("/ideas", ideasRouter);
+apiRouter.use("/meetings", meetingsRouter);
 
 module.exports = apiRouter;
