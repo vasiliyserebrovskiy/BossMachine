55a76b07403c34c48cc086416fbe65a9c09bc763
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-05-02 15:05:28 +0200

    all work request was added

--- server/routers/works.js
+++ server/routers/works.js
@@ -19,7 +19,53 @@ worksRouter.get("/", (req, res) => {
 });
 
 // POST /api/minions/:minionId/work to create a new work object and save it to the database.
+worksRouter.post("/", (req, res) => {
+  const minionId = req.params.minionId;
+  const hours = Number(req.body.hours);
+  const newWork = {
+    title: req.body.title,
+    description: req.body.description,
+    hours: hours,
+    minionId: minionId,
+  };
+
+  const result = db.addToDatabase("work", newWork);
+
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: "Error adding new work to the database." });
+  }
+});
 // PUT /api/minions/:minionId/work/:workId to update a single work by id.
+worksRouter.put("/:workId", (req, res) => {
+  const minionId = req.params.minionId;
+  const hours = Number(req.body.hours);
+  const updatedWork = {
+    id: req.params.workId,
+    title: req.body.title,
+    description: req.body.description,
+    hours: hours,
+    minionId: minionId,
+  };
+
+  const result = db.updateInstanceInDatabase("work", updatedWork);
+  console.log("Updated Work: ", updatedWork);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: "Error updating work in the database." });
+  }
+});
 // DELETE /api/minions/:minionId/work/:workId to delete a single work by id.
+worksRouter.delete("/:workId", (req, res) => {
+  const result = db.deleteFromDatabasebyId("work", req.params.workId);
+  console.log("Delete work: ", result);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: "Wrong id, work can not be deleted." });
+  }
+});
 
 module.exports = worksRouter;
