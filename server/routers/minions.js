1663a62db284d7fc251fbedb73c9b61656c60779
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-05-02 14:36:54 +0200

    all minions request added

--- server/routers/minions.js
+++ server/routers/minions.js
@@ -3,9 +3,8 @@ const minionsRouter = express.Router();
 //connect db.js
 const db = require("../db");
 
-//Route for minions works
+//Route for minion works
 const worksRouter = require("./works");
-
 minionsRouter.use("/:minionId/work", worksRouter);
 
 //GET /api/minions to get an array of all minions.
@@ -30,15 +29,18 @@ minionsRouter.post("/", (req, res) => {
   };
 
   const result = db.addToDatabase("minions", newMinion);
-  res.send(result);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: "Error adding minion to the database" });
+  }
 });
 
 // GET /api/minions/:minionId to get a single minion by id.
 minionsRouter.get("/:minionId", (req, res) => {
-  console.log("id = ", req.params.minionId);
   const minionId = req.params.minionId;
   const result = db.getFromDatabaseById("minions", minionId);
-  console.log("Minion: ", result);
+
   if (result) {
     res.send(result);
   } else {
@@ -47,6 +49,37 @@ minionsRouter.get("/:minionId", (req, res) => {
 });
 
 // PUT /api/minions/:minionId to update a single minion by id.
+minionsRouter.put("/:minionId", (req, res) => {
+  const salary = Number(req.body.salary);
+
+  const updatedMinion = {
+    id: req.params.minionId,
+    name: req.body.name,
+    title: req.body.title,
+    weaknesses: req.body.weaknesses,
+    salary: salary,
+  };
+
+  const result = db.updateInstanceInDatabase("minions", updatedMinion);
+
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: "Error updating minion in the database" });
+  }
+});
+
 // DELETE /api/minions/:minionId to delete a single minion by id.
+minionsRouter.delete("/:minionId", (req, res) => {
+  const minionId = req.params.minionId;
+
+  const result = db.deleteFromDatabasebyId("minions", minionId);
+
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send({ error: "Wrong id, minion can not be deleted." });
+  }
+});
 
 module.exports = minionsRouter;
